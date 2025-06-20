33bba1ea933e6d79eaa446e90a98c92200a3a16f
MRayhanKhdf <m.rayhankhdf@gmail.com>
2025-06-20 08:11:58 +0700

    first commit

---
+++ modelling.py
@@ -0,0 +1,10 @@
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset_path", type=str, default="ai_job_dataset.csv")
+    args = parser.parse_args()
+
+    # Ganti hardcoded path dengan:
+    X_train, X_test, y_train, y_test, _, _ = load_and_preprocess(args.dataset_path)
+    ...
